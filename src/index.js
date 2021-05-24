29e75d6d6c5baa6a6380c8c1e4c9b1b757e7909a
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-24 12:14:04 +0800

    done up chapter code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "pink",
+  fontSize: "20px",
+  border: "1px solid brown"
+};
+
+//to update properties of our customStyle:
+customStyle.color = "green";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
